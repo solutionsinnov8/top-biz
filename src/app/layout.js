f5b38a10b403fd1654a87c7fdb3b266f78e5af33
Zeeshan <mzeeshi187@gmail.com>
2025-06-25 20:31:06 +0500

    contact us form submission done

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import Footer from "./shared/Footer";
 
 
 export const metadata = {
-  title: "Contact Us - Top Biz LLP",
+  title: "Top Biz LLP",
   description: "Reach out to Top Biz LLP for expert General Order Supplies, Import-Export solutions, and Government & Defense contracting services. Get in touch today for tailored, high-quality business support in Rawalpindi, Pakistan.",
 };
 
