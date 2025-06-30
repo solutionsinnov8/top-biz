171acb949cc923f037cd88378ebb23f73778b42f
Zeeshan <mzeeshi187@gmail.com>
2025-06-30 17:51:29 +0500

    images section added

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import GeneralHeroSection from '../shared/GeneralHeroSection'
+import GallerySection from '../components/gallery/GallerySection'
 
 const page = () => {
   return (
     <div className='pt-[65px]'>
       <GeneralHeroSection title="Our Gallery" />
+      <GallerySection />
     </div>
   )
 }
