15e37af7a4eae19ec690b9c3a4e338d5721bce3f
Zeeshan <mzeeshi187@gmail.com>
2025-06-30 18:45:15 +0500

    favicon updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,18 +3,17 @@ import "./globals.css";
 import Navbar from "./shared/Navbar";
 import Footer from "./shared/Footer";
 
-
 export const metadata = {
   title: "Top Biz LLP",
-  description: "Reach out to Top Biz LLP for expert General Order Supplies, Import-Export solutions, and Government & Defense contracting services. Get in touch today for tailored, high-quality business support in Rawalpindi, Pakistan.",
+  description:
+    "Reach out to Top Biz LLP for expert General Order Supplies, Import-Export solutions, and Government & Defense contracting services. Get in touch today for tailored, high-quality business support in Rawalpindi, Pakistan.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`antialiased`}
-      >
+      <link rel="icon" href="/logo.png" />
+      <body className={`antialiased`}>
         <Navbar />
         {children}
         <Footer />
